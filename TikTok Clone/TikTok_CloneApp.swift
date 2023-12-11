9ede3a1a2dee6b39ce8b56a136352636d1da5b95
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-12-10 22:43:38 -0800

    Initial Commit

---
+++ TikTok Clone/TikTok_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTok_CloneApp.swift
+//  TikTok Clone
+//
+//  Created by Ikbal Demirdoven on 2023-12-10.
+//
+
+import SwiftUI
+
+@main
+struct TikTok_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
