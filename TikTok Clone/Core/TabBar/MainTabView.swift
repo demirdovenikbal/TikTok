23fbddf5f5c3127669516bc496763dde798e4b41
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-12-11 00:14:22 -0800

    implemented tab bar view

---
+++ TikTok Clone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  TikTok Clone
+//
+//  Created by Ikbal Demirdoven on 2023-12-10.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
