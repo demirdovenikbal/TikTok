8d29681e585a0716693e81d209fa7687609876bb
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-12-11 23:51:40 -0800

    small changes

---
+++ TikTok Clone/Core/Feed/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  TikTok Clone
+//
+//  Created by Ikbal Demirdoven on 2023-12-11.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
