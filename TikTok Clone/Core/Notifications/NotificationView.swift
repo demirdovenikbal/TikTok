9fe48214124dfee64428c24213022d65d967b4a8
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-12-13 21:50:01 -0800

    implemented notifications view

---
+++ TikTok Clone/Core/Notifications/NotificationView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationView.swift
+//  TikTok Clone
+//
+//  Created by Ikbal Demirdoven on 2023-12-13.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationView()
+}
---
+++ TikTok Clone/Core/Notifications/NotificationsView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationsView.swift
+//  TikTok Clone
+//
+//  Created by Ikbal Demirdoven on 2023-12-13.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationsView()
+}
