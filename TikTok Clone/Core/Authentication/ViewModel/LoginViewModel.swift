0150c359b4a9e841a76f4e6659ec73a0a79b924c
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-12-20 21:53:14 -0800

    implemented authentication service, login viewmodel and registration viewmodel

---
+++ TikTok Clone/Core/Authentication/ViewModel/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  TikTok Clone
+//
+//  Created by Ikbal Demirdoven on 2023-12-20.
+//
+
+import Foundation
---
+++ TikTok Clone/Core/Authentication/ViewModel/RegistrationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RegistrationViewModel.swift
+//  TikTok Clone
+//
+//  Created by Ikbal Demirdoven on 2023-12-20.
+//
+
+import Foundation
