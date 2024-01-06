4e6d25ea391177e65445c870f1443394de1f8817
demirdovenikbal <ikbaldemirdoven@icloud.com>
2024-01-05 23:43:18 -0800

    user profile view for search function implemented

---
+++ TikTok Clone/Core/Profile/View/UserProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileView.swift
+//  TikTok Clone
+//
+//  Created by Ikbal Demirdoven on 2024-01-05.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfileView()
+}
